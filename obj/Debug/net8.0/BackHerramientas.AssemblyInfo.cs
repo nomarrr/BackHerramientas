a9e591582db9283678593ab6970d9330250ef554
nomarrr <nomar3030@gmail.com>
2025-10-14 17:31:57 -0700

    CambioNombre

--- obj/Debug/net8.0/BackHerramientas.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackHerramientas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackHerramientas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d99e9349f5027bb721747002f1e294a405d62f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5c6bedf08af71b00449ed77ae38bb968e75b73")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackHerramientas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackHerramientas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
